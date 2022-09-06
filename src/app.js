c2d2bbe572def84a4091047b1d0e474afda7bc89
JonaGarces <jonat.garces@gmail.com>
2022-09-06 19:53:55 +0000

    Naipes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let boton = document.getElementById("boton");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function cambiarCarta() {
+    const pinta = ["♥", "♣ ", "♠", "♦"];
+    const valor = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let sortTipos = pinta[Math.floor(Math.random() * 4)];
+    let sortNumbers = valor[Math.floor((Math.random() * 12) + 1)];
+    
+
+    let color= sortTipos === "♥" || sortTipos === "♦" ? "red" : "black";
+
+    let numbers = document.getElementById("number");
+    let topSuit = document.getElementById("topSuit");
+    let botSuit = document.getElementById("bottomSuit");
+
+    topSuit.style.color = color;
+    botSuit.style.color =color;
+
+    numbers.innerHTML = sortNumbers;
+    topSuit.innerHTML = sortTipos;
+    botSuit.innerHTML = sortTipos;
+
+}
